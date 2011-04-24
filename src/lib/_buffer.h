da1b4137ab87be951e5b2a0254dc580125aeae1f
Daemotron <mail@daemotron.net>
2011-04-24 15:02:28 +0200

    Updated buffer module to comply with coding rules (i. e. namespace conventions)

--- src/lib/_buffer.h
+++ src/lib/_buffer.h
@@ -14,8 +14,8 @@
  * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
  */
 
-#ifndef _BUFFER_H_
-#define _BUFFER_H_
+#ifndef _BWG_BUFFER_H_
+#define _BWG_BUFFER_H_
 
 #include <errno.h>
 #include <stdlib.h>
@@ -28,20 +28,20 @@ typedef struct
     uint64_t size;
     uint64_t used;
     uint64_t limit;
-} buffer_t;
+} _bwg_buffer_t;
 
-#define BUFFER_init(__buf) {      \
+#define _BWG_BUFFER_init(__buf) { \
     (__buf)->data = NULL;         \
     (__buf)->size = 0;            \
     (__buf)->used = 0;            \
     (__buf)->limit = 0;           \
 }
 
-#define BUFFER_set_limit(__buf, __limit) {      \
+#define _BWG_BUFFER_set_limit(__buf, __limit) { \
     (__buf)->limit = (__limit);                 \
 }
 
-#define BUFFER_grow(__buf, __size, __status) {                                                               \
+#define _BWG_BUFFER_grow(__buf, __size, __status) {                                                          \
     uint64_t __sz;                                                                                           \
     if ((__buf)->size < (__buf)->limit) {                                                                    \
         __sz = (((__buf)->limit - (__buf)->size) >= (__size) ? (__size) : ((__buf)->limit - (__buf)->size)); \
@@ -59,9 +59,9 @@ typedef struct
 }
 
 
-#define BUFFER_copy(__src, __dest, __status) {                    \
-    BUFFER_set_limit((__dest), (__src)->limit);                   \
-    BUFFER_grow((__dest), (__src)->size, (__status));             \
+#define _BWG_BUFFER_copy(__src, __dest, __status) {               \
+    _BWG_BUFFER_set_limit((__dest), (__src)->limit);              \
+    _BWG_BUFFER_grow((__dest), (__src)->size, (__status));        \
     if (*(__status) == 0) {                                       \
         memcpy((__dest)->data, (__src)->data, (__src)->size + 1); \
         (__dest)->size = (__src)->size;                           \
@@ -70,7 +70,7 @@ typedef struct
 }
 
 
-#define BUFFER_destroy(__buf) {            \
+#define _BWG_BUFFER_destroy(__buf) {       \
     if (NULL != (__buf)->data) {           \
         free((__buf)->data);               \
         (__buf)->data = NULL;              \
@@ -81,14 +81,14 @@ typedef struct
 }
 
 
-extern uint64_t buffer_read_append(int fd, buffer_t *buf);
-extern uint64_t buffer_read_insert(int fd, buffer_t *buf);
-extern uint64_t buffer_write_fd(int fd, const buffer_t *buf);
-extern uint64_t buffer_free_bytes(buffer_t *buf);
-extern int buffer_get_byte(buffer_t *buf, int64_t offset);
-extern uint64_t buffer_insert(buffer_t *buffer, const char *string);
-extern uint64_t buffer_insert_0(buffer_t *buffer, const char *string);
-extern uint64_t buffer_append(buffer_t *buffer, const char *string);
-extern uint64_t buffer_append_0(buffer_t *buffer, const char *string);
+uint64_t _bwg_buffer_read_append(int fd, _bwg_buffer_t *buf);
+uint64_t _bwg_buffer_read_insert(int fd, _bwg_buffer_t *buf);
+uint64_t _bwg_buffer_write_fd(int fd, const _bwg_buffer_t *buf);
+uint64_t _bwg_buffer_free_bytes(_bwg_buffer_t *buf);
+int      _bwg_buffer_get_byte(_bwg_buffer_t *buf, int64_t offset);
+uint64_t _bwg_buffer_insert(_bwg_buffer_t *buffer, const char *string);
+uint64_t _bwg_buffer_insert_0(_bwg_buffer_t *buffer, const char *string);
+uint64_t _bwg_buffer_append(_bwg_buffer_t *buffer, const char *string);
+uint64_t _bwg_buffer_append_0(_bwg_buffer_t *buffer, const char *string);
 
-#endif /* _BUFFER_H_ */
+#endif /* _BWG_BUFFER_H_ */
--- src/lib/buffer.c
+++ src/lib/buffer.c
@@ -22,19 +22,27 @@
 
 #include "_buffer.h"
 
-extern uint64_t
-buffer_free_bytes(buffer_t *buf)
+
+/**
+ * @brief calculate free buffer size
+ */
+uint64_t
+_bwg_buffer_free_bytes(_bwg_buffer_t *buf)
 {
     return (buf->size - buf->used);
 }
 
-extern uint64_t
-buffer_read_append(int fd, buffer_t *buf)
+
+/**
+ * @brief read from file pointer and append read bytes to buffer
+ */
+uint64_t
+_bwg_buffer_read_append(int fd, _bwg_buffer_t *buf)
 {
     ssize_t rbytes;
     unsigned char *bf = (unsigned char *)buf->data;
 
-    rbytes = read(fd, &bf[buf->used], (size_t)buffer_free_bytes(buf));
+    rbytes = read(fd, &bf[buf->used], (size_t)_bwg_buffer_free_bytes(buf));
     buf->used += (uint64_t)rbytes;
 
     /* this works because the real size of buf->data is alway buf->size +1 */
@@ -43,8 +51,11 @@ buffer_read_append(int fd, buffer_t *buf)
 }
 
 
-extern uint64_t
-buffer_read_insert(int fd, buffer_t *buf)
+/**
+ * @brief read from file pointer and insert read bytes at beginning of buffer
+ */
+uint64_t
+_bwg_buffer_read_insert(int fd, _bwg_buffer_t *buf)
 {
     ssize_t rbytes;
     char *bf = (char *)buf->data;
@@ -52,7 +63,7 @@ buffer_read_insert(int fd, buffer_t *buf)
     memset(buf->data, '\0', buf->size + 1);
     buf->used = 0;
 
-    rbytes = read(fd, buf->data, (size_t)buffer_free_bytes(buf));
+    rbytes = read(fd, buf->data, (size_t)_bwg_buffer_free_bytes(buf));
     buf->used = (uint64_t)rbytes;
 
     /* this works because the real size of buf->data is alway buf->size +1 */
@@ -61,8 +72,11 @@ buffer_read_insert(int fd, buffer_t *buf)
 }
 
 
-extern uint64_t
-buffer_write_fd(int fd, const buffer_t *buf)
+/**
+ * @brief write entire buffer content to a file pointer
+ */
+uint64_t
+_bwg_buffer_write_fd(int fd, const _bwg_buffer_t *buf)
 {
     ssize_t rbytes;
 
@@ -71,8 +85,11 @@ buffer_write_fd(int fd, const buffer_t *buf)
 }
 
 
-extern uint64_t
-buffer_insert(buffer_t *buffer, const char *string)
+/**
+ * @brief insert string at beginning of buffer without incorporating a terminator symbol in the buffer content
+ */
+uint64_t
+_bwg_buffer_insert(_bwg_buffer_t *buffer, const char *string)
 {
     char *sptr = (char *)buffer->data;
     uint64_t abytes;
@@ -88,11 +105,14 @@ buffer_insert(buffer_t *buffer, const char *string)
 }
 
 
-extern uint64_t
-buffer_insert_0(buffer_t *buffer, const char *string)
+/**
+ * @brief insert string at beginning of buffer and append a terminator symbol within the buffer content range
+ */
+uint64_t
+_bwg_buffer_insert_0(_bwg_buffer_t *buffer, const char *string)
 {
     char *sptr = (char *)buffer->data;
-    uint64_t rval = buffer_insert(buffer, string);
+    uint64_t rval = _bwg_buffer_insert(buffer, string);
     if (buffer->used < buffer->size)
     {
         buffer->used += 1;
@@ -102,13 +122,16 @@ buffer_insert_0(buffer_t *buffer, const char *string)
 }
 
 
-extern uint64_t
-buffer_append(buffer_t *buffer, const char *string)
+/**
+ * @brief append string to buffer content without incorporating a terminator symbol in the buffer content
+ */
+uint64_t
+_bwg_buffer_append(_bwg_buffer_t *buffer, const char *string)
 {
     char *sptr = (char *)buffer->data;
     uint64_t abytes;
 
-    abytes = (strlen(string) < buffer_free_bytes(buffer) ? strlen(string) : buffer_free_bytes(buffer));
+    abytes = (strlen(string) < _bwg_buffer_free_bytes(buffer) ? strlen(string) : _bwg_buffer_free_bytes(buffer));
     strncpy(&sptr[buffer->used], string, abytes);
     buffer->used += abytes;
     sptr[buffer->used] = '\0';
@@ -116,11 +139,14 @@ buffer_append(buffer_t *buffer, const char *string)
 }
 
 
-extern uint64_t
-buffer_append_0(buffer_t *buffer, const char *string)
+/**
+ * @brief append string to buffer content and append a terminator symbol within the buffer content range
+ */
+uint64_t
+_bwg_buffer_append_0(_bwg_buffer_t *buffer, const char *string)
 {
     char *sptr = (char *)buffer->data;
-    uint64_t rval = buffer_append(buffer, string);
+    uint64_t rval = _bwg_buffer_append(buffer, string);
     if (buffer->used < buffer->size)
     {
         buffer->used += 1;
@@ -130,8 +156,12 @@ buffer_append_0(buffer_t *buffer, const char *string)
 }
 
 
-extern int
-buffer_get_byte(buffer_t *buf, int64_t offset)
+/**
+ * @brief get a specific byte from the buffer's content
+ * @return int
+ */
+int
+_bwg_buffer_get_byte(_bwg_buffer_t *buf, int64_t offset)
 {
     int64_t ofs;
     unsigned char *bf = (unsigned char *)buf->data;
